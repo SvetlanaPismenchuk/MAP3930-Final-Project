3abbc5115745ba997831682528255301ffd3dd9e
Adam Rumpf <arumpf@floridapoly.edu>
2026-04-10 09:21:02 -0400

    Creating a placeholder test script.

---
+++ test.py
@@ -0,0 +1,71 @@
+"""Tests for the various modules included in this repo."""
+
+import matplotlib.pyplot as plt
+
+### Import modules here ###
+
+#------------------------------------------------------------------------------
+
+def differentiation_test():
+    """Tests for the numerical differentiation module."""
+    
+    pass
+    
+    ### Tests with a test function
+    
+    ### Tests with numerical data
+
+#------------------------------------------------------------------------------
+
+def integration_test():
+    """Tests for the numerical integration module."""
+    
+    pass
+    
+    ### Tests with a test function
+    
+    ### Tests with numerical data
+
+#------------------------------------------------------------------------------
+
+def root_test():
+    """Tests for the numerical root-finding module."""
+    
+    pass
+    
+    ### Tests for bisection
+    
+    ### Tests for Newton
+    
+    ### Tests for Secant
+
+#------------------------------------------------------------------------------
+
+def linalg_test():
+    """Tests for the numerical linear algebra module."""
+    
+    pass
+    
+    ### Tests with a square system
+    
+    ### Tests with an overdetermined system
+
+#------------------------------------------------------------------------------
+
+def regression_test():
+    """Tests for the numerical regression and interpolation module."""
+    
+    pass
+    
+    ### Tests for finding an interpolating polynomial
+    
+    ### Tests for finding a best-fit polynomial
+
+#==============================================================================
+
+# Run scripts (comment out individual tests to skip)
+differentiation_test()
+integration_test()
+root_test()
+linalg_test()
+regression_test()
